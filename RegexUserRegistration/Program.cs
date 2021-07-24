61d9c92863937c4c7b1ba1704cc4c869e3c03700
Shashank <shashankpateriya4@gmail.com>
2021-07-24 13:35:27 +0530

    Add project files.

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RegexUserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            Console.WriteLine("First letter of name should be capital.");
+            Console.WriteLine("Length of name should be minimum three characters.");
+            UserRegistration validateUser = new UserRegistration();
+            validateUser.validateFirstName("Shashank");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RegexUserRegistration/UserRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUserRegistration
+{
+    class UserRegistration
+    {
+        public void validateFirstName(string firstName)
+        {
+            string stringForFirstName = "^[A-Z][a-z]{3,}$";
+            if (Regex.IsMatch(firstName, stringForFirstName))
+                Console.WriteLine(firstName + " is Valid");
+            else
+                Console.WriteLine(firstName + " is Invalid");
+        }
+    }
+}
